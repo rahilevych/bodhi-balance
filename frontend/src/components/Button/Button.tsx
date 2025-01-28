800136e544eb661fc62799f57be9610fe7ebaab5
AlonaRahilevych <ragilevic@gmail.com>
2025-01-28 14:44:40 +0100

    feat: add Button component
    Created a reusable button component with customizable text and an icon. The component supports hover effects and can be styled through CSS modules.

---
+++ frontend/src/components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './Button.module.css';
+import { FiChevronRight } from 'react-icons/fi';
+
+interface Props {
+  text: string;
+}
+const Button: React.FC<Props> = ({ text }) => {
+  return (
+    <div className={styles.button}>
+      <span>{text}</span>
+      <FiChevronRight className={styles.icon} />
+    </div>
+  );
+};
+
+export default Button;

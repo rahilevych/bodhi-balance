de83a5e0519c886075f2a2d864f78a63c192f9cf
AlonaRahilevych <ragilevic@gmail.com>
2025-04-13 21:30:18 +0200

    feat: create YogaStyles section

--- frontend/src/components/Slider/Slider.tsx
+++ frontend/src/components/Slider/Slider.tsx
@@ -3,21 +3,38 @@ import React, { useEffect, useState } from 'react';
 import styles from './Slider.module.css';
 
 import SliderNav from '../SliderNavigation/SliderNav';
+import { YogaCardType } from '../../types/Types';
 
-interface Props<T> {
-  items: T[];
-  renderCard: (item: T) => React.ReactNode;
+interface Props {
+  items: YogaCardType[];
+  renderCard: (item: YogaCardType) => React.ReactNode;
+  currentCard: YogaCardType;
+  onChange: (card: YogaCardType) => void;
 }
-const Slider = <T,>({ items, renderCard }: Props<T>) => {
-  const [index, setIndex] = useState(0);
+const Slider = ({ items, renderCard, currentCard, onChange }: Props) => {
+  const [index, setIndex] = useState(
+    items.findIndex((item) => item.id === currentCard.id)
+  );
   const [progress, setProgress] = useState(0);
 
   const handleNextBtn = () => {
-    setIndex((prev) => (prev + 1) % items.length);
+    setIndex((prev) => {
+      const newIndex = (prev + 1) % items.length;
+      onChange(items[newIndex]);
+      return newIndex;
+    });
   };
   const handlePrevBtn = () => {
-    setIndex((prev) => (prev === 0 ? items.length - 1 : prev - 1));
+    setIndex((prev) => {
+      const newIndex = prev === 0 ? items.length - 1 : prev - 1;
+      onChange(items[newIndex]);
+      return newIndex;
+    });
   };
+  useEffect(() => {
+    const currentIndex = items.findIndex((item) => item.id === currentCard.id);
+    setIndex(currentIndex);
+  }, [currentCard, items]);
   useEffect(() => {
     const interval = setInterval(() => {
       handleNextBtn();
@@ -25,9 +42,13 @@ const Slider = <T,>({ items, renderCard }: Props<T>) => {
 
     return () => clearInterval(interval);
   }, [index]);
+  useEffect(() => {
+    const calculatedProgress = ((index + 1) / items.length) * 100;
+    setProgress(calculatedProgress);
+  }, [index, items.length]);
   return (
     <div className={styles.slider}>
-      {renderCard(items[index])}
+      {renderCard(currentCard)}
       <div className={styles.nav}>
         {' '}
         <SliderNav

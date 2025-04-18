53fc7ad18b1b787d6ac40589a2d498dec67b3793
AlonaRahilevych <ragilevic@gmail.com>
2025-04-18 18:41:39 +0200

    feat:create pricing card

---
+++ frontend/src/components/Pricing/PricingCard.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
+import styles from './PricingCard.module.css';
+interface Props {
+  plan: any;
+}
+export const PricingCard = ({ plan }: Props) => {
+  const [selectedOption, setSelectedOption] = useState(plan.options[0]);
+
+  const handleOptionChange = (selectedOption: string) => {
+    setSelectedOption(selectedOption);
+  };
+  return (
+    <div className={styles.card}>
+      <h3>{plan.category}</h3>
+      <div className={styles.categories}>
+        {plan.options &&
+          plan.options.map((option, index) => (
+            <p
+              className={selectedOption === option ? styles.active : ''}
+              onClick={() => handleOptionChange(option)}>
+              {option.title}
+            </p>
+          ))}
+      </div>
+      <div>{selectedOption.description}</div>
+      <div>Price: {selectedOption.price}</div>
+      <div>
+        {' '}
+        <Button text='Book now' />
+      </div>
+    </div>
+  );
+};

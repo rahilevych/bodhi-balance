bd652ee209c559a2b4e3add95bbb22de657b5ffc
AlonaRahilevych <ragilevic@gmail.com>
2025-04-29 19:37:01 +0200

    feat: create full card for trainer

---
+++ frontend/src/components/Teachers/FullCard.tsx
@@ -0,0 +1,21 @@
+import { Teacher } from '../../types/Types';
+import styles from './FullCard.module.css';
+interface Props {
+  currentTeacher: Teacher;
+}
+
+export const FullCard = ({ currentTeacher }: Props) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.photo}>
+        <img src={currentTeacher.photo} alt='' />
+      </div>
+      <div className={styles.info}>
+        <h4>{currentTeacher.fullName}</h4>
+        <p>{currentTeacher.experience}</p>
+        <p>{currentTeacher.specialization}</p>
+        <p>{currentTeacher.about}</p>
+      </div>
+    </div>
+  );
+};

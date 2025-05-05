ac17a04e3107a61257f4e5eef82378d9bfbd1eb5
AlonaRahilevych <ragilevic@gmail.com>
2025-05-05 13:49:26 +0200

    feat: create login page

--- frontend/src/pages/LoginPage/Login.tsx
+++ frontend/src/pages/LoginPage/Login.tsx
@@ -1,7 +1,16 @@
 import styles from './Login.module.css';
+import { LoginForm } from './LoginForm';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.login}>
+      <h4>Sign in</h4>
+      <LoginForm />
+      <p>
+        Don't have an account? <span className={styles.link}> Sign up</span>
+      </p>
+    </div>
+  );
 };
 
 export default Login;

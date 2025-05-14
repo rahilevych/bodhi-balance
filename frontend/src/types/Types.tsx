bdd34d786581ffb044ee623c3d58365748fd50eb
AlonaRahilevych <ragilevic@gmail.com>
2025-05-14 15:38:31 +0200

    feat: add user type

--- frontend/src/types/Types.tsx
+++ frontend/src/types/Types.tsx
@@ -6,6 +6,14 @@ export type YogaCardType = {
   days: string[];
   description: String;
 };
+export type User = {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: Date;
+};
 export type Teacher = {
   id: string;
   fullName: string;

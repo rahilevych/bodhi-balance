43272fc0ee582a7ca076feadcf0df4c512b58af6
AlonaRahilevych <ragilevic@gmail.com>
2025-05-21 14:46:07 +0200

    refactor:move data from about section

---
+++ frontend/src/constants/about.ts
@@ -0,0 +1,26 @@
+export const aboutInfo = {
+  info: "Welcome to Bodhi Balance, a serene space where mind, body, and soul unite. Our studio is designed to provide a peaceful and inspiring environment for your yoga journey, whether you're a beginner or an experienced practitioner.",
+  img: 'https://i.pinimg.com/236x/41/fd/27/41fd27979325f866d134b077de5d9bca.jpg',
+  links: [
+    {
+      name: '8 Yoga styles',
+      link: 'yogastyles',
+    },
+    {
+      name: 'Certified instructors with years of practice and teaching experience',
+      link: 'trainers',
+    },
+    {
+      name: ' A warm and welcoming space designed for comfort and mindfulness',
+      link: 'atmosphere',
+    },
+    {
+      name: ' Morning, afternoon, and evening sessions to fit your lifestyle',
+      link: 'schedule',
+    },
+    {
+      name: 'Choose from single sessions, memberships, or package deals',
+      link: 'price',
+    },
+  ],
+};

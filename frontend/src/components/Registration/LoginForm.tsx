4fbfe8e2d740ca9d573e7cd2bae5236efbf73ebc
AlonaRahilevych <ragilevic@gmail.com>
2025-05-21 13:47:50 +0200

    test:cover login form with tests

--- frontend/src/components/Registration/LoginForm.tsx
+++ frontend/src/components/Registration/LoginForm.tsx
@@ -9,7 +9,7 @@ import { useAppContext } from '../../context/AppContext';
 import { loginUser } from '../../services/authService';
 
 const schema = z.object({
-  email: z.string().email('Invalid email format').min(1, 'Email is required !'),
+  email: z.string().min(1, 'Email is required!').email('Invalid email format'),
   password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
 

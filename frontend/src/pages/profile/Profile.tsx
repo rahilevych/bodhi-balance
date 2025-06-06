11b3beab540d9026afafa4663970d304a6c379b2
AlonaRahilevych <ragilevic@gmail.com>
2025-06-06 22:18:54 +0200

    feat:update profile page

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,7 +1,22 @@
+import { ProfileHeader } from '../../components/Header/ProfileHeader';
+import { ProfileNav } from '../../components/Nav/ProfileNav';
+import { PersonalData } from '../../components/profile/PersonalData';
 import styles from './Profile.module.css';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className={styles.profile}>
+      <div className='container'>
+        <div className={styles.content}>
+          <ProfileNav />
+          <div className={styles.info}>
+            <ProfileHeader />
+            <PersonalData />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;

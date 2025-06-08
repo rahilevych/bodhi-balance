e3be074657ed1b2b4a594cc5701835efb8e92953
AlonaRahilevych <ragilevic@gmail.com>
2025-06-08 21:57:30 +0200

    refactor: move img from header into nav in profile page

--- frontend/src/components/Header/ProfileHeader.tsx
+++ frontend/src/components/Header/ProfileHeader.tsx
@@ -1,15 +1,12 @@
 import styles from './ProfileHeader.module.css';
 
-export const ProfileHeader = () => {
+type ProfileHaederProps = {
+  text: string;
+};
+export const ProfileHeader = ({ text }: ProfileHaederProps) => {
   return (
     <div className={styles.header}>
-      <div className={styles.img}>
-        <img
-          src='https://i.pinimg.com/736x/07/fb/34/07fb3452c4640d881a16d08c2e314f3e.jpg'
-          alt=''
-        />
-      </div>
-      <div className={styles.title}>Welcome back, user</div>
+      <div className={styles.title}>{text}</div>
     </div>
   );
 };

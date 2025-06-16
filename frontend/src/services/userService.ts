013936826c5c44ebaed72c34e3a4077037a5572b
AlonaRahilevych <ragilevic@gmail.com>
2025-06-16 14:22:25 +0200

    refactor(user): add credentials for user update

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -3,8 +3,12 @@ import { UserFormData } from '../components/profile/PersonalData';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const updateUser = async (data: UserFormData, id: string) => {
-  const res = await axios.put(`${BASE_URL}/users/${id}`, {
-    data,
-  });
+  const res = await axios.put(
+    `${BASE_URL}/users/${id}`,
+    {
+      data,
+    },
+    { withCredentials: true }
+  );
   return res.data;
 };

8daf680df42dc9729a151881395a0d1c90aecd6c
AlonaRahilevych <ragilevic@gmail.com>
2025-06-16 14:21:37 +0200

    feat(utils): add helpers for data and time convertion

--- frontend/src/utils/dateHelpers.ts
+++ frontend/src/utils/dateHelpers.ts
@@ -13,9 +13,26 @@ export function getNext7Days(): Date[] {
 
 export const getTimeFromDate = (datetime: any) => {
   const date = new Date(datetime);
-  const hours = date.getUTCHours();
-  const minutes = date.getUTCMinutes();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
   const formattedMinutes = minutes.toString().padStart(2, '0');
   const time = `${hours}:${formattedMinutes}`;
   return time;
+};
+export const getEndTime = (datetime: Date, duration: number) => {
+  const startDate = new Date(datetime);
+  console.log(startDate);
+  const endDate = new Date(startDate.getTime() + duration * 60000);
+  console.log(endDate);
+  const hours = endDate.getHours();
+  const minutes = endDate.getMinutes();
+  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const time = `${hours}:${formattedMinutes}`;
+  return time;
+};
+export const convertDateToString = (datetime: any) => {
+  const date = new Date(datetime);
+  const options = { month: 'long', day: 'numeric' } as const;
+  const formattedDate = date.toLocaleDateString('en-US', options);
+  return formattedDate;
 };

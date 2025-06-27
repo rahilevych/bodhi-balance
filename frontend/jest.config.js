6f6c6f64931860e3ad7064e6f0760bbb1c9f9403
AlonaRahilevych <ragilevic@gmail.com>
2025-06-27 19:01:08 +0200

    chore: configure jest and setup tests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -5,16 +5,14 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} */
 export default {
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
   transform: {
     ...tsJestTransformCfg,
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 
   moduleNameMapper: {
-    // Маппинг для CSS-модулей — используем identity-obj-proxy
     '\\.module\\.css$': 'identity-obj-proxy',
-
-    // Мок для обычных CSS, если есть
     '\\.css$': '<rootDir>/__mocks__/styleMock.js',
   },
 };

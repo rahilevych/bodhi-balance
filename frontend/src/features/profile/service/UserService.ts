1daa86a4c11835cb9a319ef95678114d543a6c64
AlonaRahilevych <ragilevic@gmail.com>
2025-11-23 18:53:21 +0100

    feat: create  UserService class

---
+++ frontend/src/features/profile/service/UserService.ts
@@ -0,0 +1,15 @@
+import api from '../../../shared/api/axiosInstance';
+import { UserFormData } from '../components/personal-data/PersonalData';
+
+export default class UserService {
+  static async updateUser(data: UserFormData, id: string) {
+    const res = await api.put(`/users/${id}`, {
+      data,
+    });
+    return res.data;
+  }
+  static async deleteUser() {
+    const res = await api.delete(`/users/delete`);
+    return res.data;
+  }
+}

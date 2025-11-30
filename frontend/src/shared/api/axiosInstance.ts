026e953bb3e70a88725af621c4a2abebeac34a95
AlonaRahilevych <ragilevic@gmail.com>
2025-11-30 20:28:00 +0100

    feat: add interceptor for errors

--- frontend/src/shared/api/axiosInstance.ts
+++ frontend/src/shared/api/axiosInstance.ts
@@ -11,4 +11,30 @@ const api = axios.create({
   timeout: 10000,
 });
 
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const status = error.response.status;
+
+      if (status === 401) {
+        console.warn('Unauthorized');
+      }
+
+      return Promise.reject({
+        status,
+        message: error.response.data?.message || 'Error occurred',
+      });
+    }
+    if (error.request) {
+      return Promise.reject({
+        message: 'Check your internet connection',
+      });
+    }
+    return Promise.reject({
+      message: error.message || 'Unexpected error',
+    });
+  },
+);
+
 export default api;

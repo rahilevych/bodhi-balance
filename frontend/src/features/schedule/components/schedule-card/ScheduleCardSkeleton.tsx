8a2eb21944a3b345be3dc5fb588accae87a7edc4
AlonaRahilevych <ragilevic@gmail.com>
2025-11-30 15:20:56 +0100

    refactor: update schedule card

--- frontend/src/features/schedule/components/schedule-card/ScheduleCardSkeleton.tsx
+++ frontend/src/features/schedule/components/schedule-card/ScheduleCardSkeleton.tsx
@@ -4,8 +4,14 @@ import styles from './ScheduleCard.module.css';
 export const ScheduleCardSkeleton = () => {
   return (
     <div className={styles.card}>
-      <Skeleton width={150} height={16} count={5} />
-      <Skeleton width={150} height={40} />
+      <div className={styles.info}>
+        <Skeleton width={150} height={21} />
+        <Skeleton width={150} height={16} count={3} />
+      </div>
+      <div className={styles.nav}>
+        <Skeleton width={150} height={16} />
+        <Skeleton width={150} height={40} />
+      </div>
     </div>
   );
 };

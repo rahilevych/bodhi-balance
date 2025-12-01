cc95e9ffc592955e2cc56eb1897891202c4abe0e
AlonaRahilevych <ragilevic@gmail.com>
2025-12-01 21:28:09 +0100

    test: update tests for Registrtion

--- frontend/src/features/auth/__tests__/Registration.test.tsx
+++ frontend/src/features/auth/__tests__/Registration.test.tsx
@@ -1,156 +1,113 @@
-// import { render, screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import '@testing-library/jest-dom';
-// import { useAppContext } from '../../../context/AppContext';
-// import { registerUser } from '../../../services/authService';
-// import { RegistrationForm } from './RegistrationForm';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { RegistrationForm } from '../components/registration/RegistrationForm';
+import { useRegistration } from '../hooks/useRegistration';
 
-// jest.mock('../../../context/AppContext.tsx', () => ({
-//   useAppContext: jest.fn(),
-// }));
-// jest.mock('../../../services/authService.ts', () => ({
-//   registerUser: jest.fn(),
-// }));
+const mutateMock = jest.fn();
+const navigateMock = jest.fn();
+jest.mock('../hooks/useRegistration.ts', () => ({
+  useRegistration: jest.fn(() => ({
+    mutate: jest.fn(),
+    isPending: false,
+  })),
+}));
 
-// describe('RegistrationForm', () => {
-//   const setNotificationMock = jest.fn();
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useNavigate: () => navigateMock,
+}));
 
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//     (useAppContext as jest.Mock).mockReturnValue({
-//       setNotification: setNotificationMock,
-//     });
-//   });
+describe('RegistrationForm', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//   test('renders form inputs and button', () => {
-//     render(<RegistrationForm />);
-//     const textInputs = screen.getAllByRole('textbox');
-//     const passwordInput = screen.getByPlaceholderText(/password/i);
-//     expect(textInputs).toHaveLength(2);
-//     expect(passwordInput).toBeInTheDocument();
-//     expect(
-//       screen.getByRole('button', { name: /sign up/i })
-//     ).toBeInTheDocument();
-//   });
+  test('renders form inputs and button', () => {
+    render(<RegistrationForm />);
+    const textInputs = screen.getAllByRole('textbox');
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    expect(textInputs).toHaveLength(2);
+    expect(passwordInput).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /sign up/i }),
+    ).toBeInTheDocument();
+  });
 
-//   test('shows validation errors on empty submit', async () => {
-//     render(<RegistrationForm />);
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     expect(await screen.findByText('Name is required!')).toBeInTheDocument();
-//     expect(await screen.findByText('Email is required!')).toBeInTheDocument();
-//     expect(
-//       await screen.findByText('Password must be at least 6 characters long')
-//     ).toBeInTheDocument();
-//   });
-//   test('shows error for invalid email format', async () => {
-//     render(<RegistrationForm />);
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/full name/i),
-//       'Adam Smith'
-//     );
-//     await userEvent.type(screen.getByPlaceholderText(/email/i), ' smith');
-//     await userEvent.type(screen.getByPlaceholderText(/password/i), ' 111111');
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     expect(
-//       await screen.findByText(/Invalid email format/i)
-//     ).toBeInTheDocument();
-//   });
-//   test('shows error for invalid password format', async () => {
-//     render(<RegistrationForm />);
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/full name/i),
-//       'Adam Smith'
-//     );
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/email/i),
-//       ' smith@gmail.com'
-//     );
-//     await userEvent.type(screen.getByPlaceholderText(/password/i), ' 1111');
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     expect(
-//       await screen.findByText(/Password must be at least 6 characters long/i)
-//     ).toBeInTheDocument();
-//   });
-//   test('calls registerUser from service and show that registration was successful', async () => {
-//     (registerUser as jest.Mock).mockResolvedValueOnce({});
-//     render(<RegistrationForm />);
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/full name/i),
-//       'Adam Smith'
-//     );
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/email/i),
-//       'smith@gmail.com'
-//     );
-//     await userEvent.type(screen.getByPlaceholderText(/password/i), '111111');
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     await waitFor(() => {
-//       expect(registerUser).toHaveBeenCalledWith({
-//         fullName: 'Adam Smith',
-//         email: 'smith@gmail.com',
-//         password: '111111',
-//       });
-//     });
-//     expect(setNotificationMock).toHaveBeenCalledWith(
-//       'Successfully registered!'
-//     );
-//   });
-//   test('shows error message when registration failed', async () => {
-//     const error = {
-//       response: { data: { error: 'User already exists' } },
-//     };
-//     (registerUser as jest.Mock).mockRejectedValueOnce(error);
-//     render(<RegistrationForm />);
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/full name/i),
-//       'Adam Smith'
-//     );
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/email/i),
-//       'smith@gmail.com'
-//     );
-//     await userEvent.type(screen.getByPlaceholderText(/password/i), '111111');
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     await waitFor(() => {
-//       expect(registerUser).toHaveBeenCalledWith({
-//         fullName: 'Adam Smith',
-//         email: 'smith@gmail.com',
-//         password: '111111',
-//       });
-//     });
-//     expect(setNotificationMock).toHaveBeenCalledWith('Registration failed!');
-//   });
+  test('shows validation errors on empty submit', async () => {
+    render(<RegistrationForm />);
+    const button = screen.getByRole('button', { name: /sign up/i });
+    await userEvent.click(button);
+    expect(await screen.findByText('Name is required!')).toBeInTheDocument();
+    expect(await screen.findByText('Email is required!')).toBeInTheDocument();
+    expect(
+      await screen.findByText('Password must be at least 6 characters long'),
+    ).toBeInTheDocument();
+  });
+  test('shows error for invalid email format', async () => {
+    render(<RegistrationForm />);
+    await userEvent.type(
+      screen.getByPlaceholderText(/full name/i),
+      'Adam Smith',
+    );
+    await userEvent.type(screen.getByPlaceholderText(/email/i), ' smith');
+    await userEvent.type(screen.getByPlaceholderText(/password/i), ' 111111');
+    const button = screen.getByRole('button', { name: /sign up/i });
+    await userEvent.click(button);
+    expect(
+      await screen.findByText(/Invalid email format/i),
+    ).toBeInTheDocument();
+  });
+  test('shows error for invalid password format', async () => {
+    render(<RegistrationForm />);
+    await userEvent.type(
+      screen.getByPlaceholderText(/full name/i),
+      'Adam Smith',
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText(/email/i),
+      ' smith@gmail.com',
+    );
+    await userEvent.type(screen.getByPlaceholderText(/password/i), ' 1111');
+    const button = screen.getByRole('button', { name: /sign up/i });
+    await userEvent.click(button);
+    expect(
+      await screen.findByText(/Password must be at least 6 characters long/i),
+    ).toBeInTheDocument();
+  });
+  test('calls mutate and navigate on valid submit', async () => {
+    (useRegistration as jest.Mock).mockReturnValue({
+      mutate: mutateMock,
+      isPending: false,
+    });
+    render(<RegistrationForm />);
+    await userEvent.type(
+      screen.getByPlaceholderText(/full name/i),
+      'Adam Smith',
+    );
+    await userEvent.type(
+      screen.getByPlaceholderText(/email/i),
+      'smith@gmail.com',
+    );
+    await userEvent.type(screen.getByPlaceholderText(/password/i), '111111');
+    const button = screen.getByRole('button', { name: /sign up/i });
+    await userEvent.click(button);
+    await waitFor(() => {
+      expect(mutateMock).toHaveBeenCalledWith({
+        fullName: 'Adam Smith',
+        email: 'smith@gmail.com',
+        password: '111111',
+      });
+    });
+    expect(navigateMock).toHaveBeenCalledWith('/');
+  });
 
-//   test('shows unknown error message on unexpected error', async () => {
-//     (registerUser as jest.Mock).mockRejectedValueOnce(
-//       new Error('Network Error')
-//     );
-//     render(<RegistrationForm />);
+  test('renders Loader when isPending is true', () => {
+    (useRegistration as jest.Mock).mockReturnValue({
+      mutate: jest.fn(),
+      isPending: true,
+    });
 
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/full name/i),
-//       'Adam Smith'
-//     );
-//     await userEvent.type(
-//       screen.getByPlaceholderText(/email/i),
-//       'smith@gmail.com'
-//     );
-//     await userEvent.type(screen.getByPlaceholderText(/password/i), '111111');
-//     const button = screen.getByRole('button', { name: /sign up/i });
-//     await userEvent.click(button);
-//     await waitFor(() => {
-//       expect(registerUser).toHaveBeenCalledWith({
-//         fullName: 'Adam Smith',
-//         email: 'smith@gmail.com',
-//         password: '111111',
-//       });
-//     });
-//     expect(await screen.findByText('Unknown error')).toBeInTheDocument();
-//     expect(setNotificationMock).toHaveBeenCalledWith('Registration failed!');
-//   });
-// });
+    render(<RegistrationForm />);
+    expect(screen.getByTestId('loader')).toBeInTheDocument();
+  });
+});

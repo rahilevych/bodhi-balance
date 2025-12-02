24af4836d7a60c70266ab36053ffa10d76e96f21
AlonaRahilevych <ragilevic@gmail.com>
2025-12-02 20:21:06 +0100

    test: update test for nav

--- frontend/src/features/nav/__tests__/Nav.test.tsx
+++ frontend/src/features/nav/__tests__/Nav.test.tsx
@@ -1,92 +1,41 @@
 import { render, screen } from '@testing-library/react';
-import Nav from '../components/nav/Nav';
 import { useAppContext } from '../../../context/AppContext';
-import { useNavigate } from 'react-router-dom';
-import '@testing-library/jest-dom';
-import { sectionsList } from '../../../constants/sections';
-import userEvent from '@testing-library/user-event';
+import Nav from '../components/nav/Nav';
 
-jest.mock('../../context/AppContext');
-jest.mock('../../services/authService.ts', () => ({
-  getMe: jest.fn(),
-}));
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
+jest.mock('../../../context/AppContext', () => ({
+  useAppContext: jest.fn(),
 }));
 
-describe('Nav', () => {
-  const mockOpenModal = jest.fn();
-  const mockNavigate = jest.fn();
-  const user = userEvent.setup();
+jest.mock('../../../shared/ui/logo/Logo', () => ({
+  Logo: () => <div>Logo</div>,
+}));
 
-  beforeEach(() => {
-    (useAppContext as jest.Mock).mockReturnValue({
-      isAuthenticated: false,
-      openModal: mockOpenModal,
-    });
-    (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
+jest.mock('../../menu/components/laptop-menu/LaptopMenu', () => ({
+  LaptopMenu: () => <div>LaptopMenu</div>,
+}));
+
+jest.mock('../../menu/components/mobile-menu/MobileMenu', () => ({
+  MobileMenu: () => <div>MobileMenu</div>,
+}));
+
+describe('Nav component', () => {
+  it('renders Logo', () => {
+    (useAppContext as jest.Mock).mockReturnValue({ isMobile: false });
+    render(<Nav />);
+    expect(screen.getByText('Logo')).toBeInTheDocument();
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-  test('renders menu sections', () => {
+  it('renders LaptopMenu when not mobile', () => {
+    (useAppContext as jest.Mock).mockReturnValue({ isMobile: false });
     render(<Nav />);
-    sectionsList.forEach((section) => {
-      expect(screen.getByText(section.name)).toBeInTheDocument();
-    });
-    expect(screen.getByText(/Bodhi balance/i)).toBeInTheDocument();
-    expect(screen.getByText(/Sign in/i)).toBeInTheDocument();
+    expect(screen.getByText('LaptopMenu')).toBeInTheDocument();
+    expect(screen.queryByText('MobileMenu')).not.toBeInTheDocument();
   });
-  test('shows subsections when main section is clicked', async () => {
-    render(<Nav />);
-    for (const section of sectionsList) {
-      const mainSection = screen.getByText(section.name);
-      await user.click(mainSection);
-      for (const subSection of section.subSections) {
-        expect(screen.getByText(subSection.name)).toBeInTheDocument();
-      }
-    }
-  });
-  test('burger menu toggles open and close', async () => {
-    render(<Nav />);
-    const burger = screen.getByTestId('burger');
 
-    expect(screen.getByTestId('burger-menu')).not.toHaveClass('menu_opened');
-
-    await user.click(burger);
-    expect(screen.getByTestId('burger-menu')).toHaveClass('menu_opened');
-
-    await user.click(burger);
-    expect(screen.getByTestId('burger-menu')).not.toHaveClass('menu_opened');
-  });
-  test('sign in button opens modal window', async () => {
+  it('renders MobileMenu when mobile', () => {
+    (useAppContext as jest.Mock).mockReturnValue({ isMobile: true });
     render(<Nav />);
-    const signInBtn = screen.getByText(/sign in/i);
-    await user.click(signInBtn);
-    expect(mockOpenModal).toHaveBeenCalled();
-  });
-  test('logo click navigates to "/"', async () => {
-    render(<Nav />);
-    await user.click(screen.getByTestId('logo'));
-    expect(mockNavigate).toHaveBeenCalledWith('/');
-  });
-  test('render "profile" and "logout " buttons if user authenticated', () => {
-    (useAppContext as jest.Mock).mockReturnValue({
-      isAuthenticated: true,
-    });
-    render(<Nav />);
-    expect(screen.getByText(/profile/i)).toBeInTheDocument();
-    expect(screen.getByText(/log out/i)).toBeInTheDocument();
-  });
-  test('profile button opens modal redirect to profile page', async () => {
-    (useAppContext as jest.Mock).mockReturnValue({
-      isAuthenticated: true,
-    });
-    render(<Nav />);
-
-    await user.click(screen.getByText(/profile/i));
-    expect(mockNavigate).toHaveBeenCalledWith('/profile');
+    expect(screen.getByText('MobileMenu')).toBeInTheDocument();
+    expect(screen.queryByText('LaptopMenu')).not.toBeInTheDocument();
   });
 });

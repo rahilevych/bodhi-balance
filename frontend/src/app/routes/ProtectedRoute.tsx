766992a595978d1c2f64182b6c904ff48c66e281
AlonaRahilevych <ragilevic@gmail.com>
2026-01-13 12:53:19 +0100

    refactor: change timeout

--- frontend/src/app/routes/ProtectedRoute.tsx
+++ frontend/src/app/routes/ProtectedRoute.tsx
@@ -1,17 +1,15 @@
 import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
-
 import { useProfile } from '../../features/auth/hooks/useProfile';
-import { Loader } from '../../shared/ui/loader/Loader';
 import { useAppContext } from '../../context/AppContext';
 
 interface ProtectedRouteProps {
   element: ReactNode;
 }
 const ProtectedRoute = ({ element }: ProtectedRouteProps) => {
-  const { data: user, isPending } = useProfile();
+  const { data: user } = useProfile();
   const { token } = useAppContext();
-  if (isPending) return <Loader />;
+
   return user && token ? element : <Navigate to='/auth' replace />;
 };
 
--- frontend/src/shared/api/axiosInstance.ts
+++ frontend/src/shared/api/axiosInstance.ts
@@ -9,7 +9,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000,
+  timeout: 1000,
 });
 
 api.interceptors.request.use((config) => {

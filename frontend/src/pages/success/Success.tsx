d2f8b90a5002aa1bc8eb3319abbaa243e1e44635
AlonaRahilevych <ragilevic@gmail.com>
2026-02-08 12:20:24 +0100

    refactor:update success page

--- frontend/src/pages/success/Success.tsx
+++ frontend/src/pages/success/Success.tsx
@@ -3,9 +3,31 @@ import styles from './Success.module.css';
 
 import { FaCheckCircle } from 'react-icons/fa';
 import Button from '../../shared/ui/button/Button';
+import { refreshAccessToken } from '../../utils/refreshAccessToken';
+import { useEffect, useState } from 'react';
+import { useAppContext } from '../../context/AppContext';
+import { Loader } from '../../shared/ui/loader/Loader';
 
 export const Success = () => {
   const navigate = useNavigate();
+  const { setToken, setIsAuth } = useAppContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const initAuth = async () => {
+      const token = await refreshAccessToken();
+      if (token) {
+        setToken(token);
+        setIsAuth(true);
+        localStorage.setItem('accessToken', token);
+      }
+      setLoading(false);
+    };
+
+    initAuth();
+  }, [setToken, setIsAuth]);
+
+  if (loading) return <Loader />;
   return (
     <div className={styles.success}>
       <div className={styles.content}>
